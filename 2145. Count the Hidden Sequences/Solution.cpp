e041f46bc52d24d8f99675f7b0a43b0fdc916c5c
Franky Wu <wuyiulin@gmail.com>
2025-04-21 21:30:59 +0800

    update 2145. Count the Hidden Sequences

---
+++ 2145. Count the Hidden Sequences/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& diff, int lower, int upper) 
+    {
+        int gap = upper - lower;
+        long long dLo = 0, dUp = 0, prefix = 0;
+        for(const auto& d : diff)
+        {
+            prefix += d;
+            dLo = min(dLo, prefix);
+            dUp = max(dUp, prefix);
+        }
+        cout<<"dLo: "<<dLo<<", dUp: "<<dUp<<"\n";
+        int calGap = dUp - dLo;
+
+        if(gap < calGap) return 0;
+
+        return gap - calGap + 1;
+    }
+};
+
