bc630d7bce9dc5650a66473664d6817a2e6a02d0
Franky Wu <wuyiulin@gmail.com>
2025-06-21 23:04:31 +0800

    update 3085. Minimum Deletions to Make String K-Special code

---
+++ 3085. Minimum Deletions to Make String K-Special/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) 
+    {
+        int res = INT_MAX;
+        unordered_map<char, int> table;
+        for(const auto& c : word)
+            table[c]++;
+        for(auto& [_, tar] : table)
+        {
+            int diff = 0;
+            for(auto& [_, curr] : table)
+            { 
+                if(curr - tar > k)
+                    diff += (curr - tar - k); 
+                else if(curr < tar)
+                    diff += curr;
+            }
+            res = min(res, diff);
+        }
+
+        return res;
+    }
+};
