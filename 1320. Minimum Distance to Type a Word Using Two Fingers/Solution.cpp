ce74534e69c6aea1e4f740a1ba0e422d4c3bc44e
wuyiulin <wuyiulin@gmail.com>
2026-04-12 23:45:49 +0800

    update rolling dp

--- 1320. Minimum Distance to Type a Word Using Two Fingers/Solution.cpp
+++ 1320. Minimum Distance to Type a Word Using Two Fingers/Solution.cpp
@@ -11,29 +11,31 @@ public:
     int minimumDistance(string word) 
     {
         int n = word.size(), res = 1e9;
-        vector<vector<int>> dp(n, vector<int>(26, 1e9));
+        vector<vector<int>> dp(2, vector<int>(26, 1e9));
         for(int i=0; i<26; i++)
             dp[0][i] = 0;
         
         for(int i=1; i<n; i++)
         {
+            fill(dp[1].begin(), dp[1].end(), 1e9);
             int prev = word[i - 1] - 'A', curr = word[i] - 'A';
             int d = getDist(prev, curr);
             for(int j=0; j<26; j++)
             {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + d);
+                dp[1][j] = min(dp[1][j], dp[0][j] + d);
                 if(prev == j)
                 {
                     for(int k=0; k<26; k++)
                     {
                         int dd = getDist(k, curr);
-                        dp[i][j] = min(dp[i][j], dp[i - 1][k] + dd);
+                        dp[1][j] = min(dp[1][j], dp[0][k] + dd);
                     }
                 }
             }
+            dp[0] = dp[1];
         }
 
-        res = *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        res = *min_element(dp[0].begin(), dp[0].end());
     
         return res;
     }
