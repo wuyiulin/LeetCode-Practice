1b244b0e5673420be94c78eebffe9ac67c8fb434
wuyiulin <wuyiulin@gmail.com>
2026-06-08 21:41:04 +0800

    update 2161. Partition Array According to Given Pivot

---
+++ 2161. Partition Array According to Given Pivot/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) 
+    {
+        vector<int> res = {};
+        for(const auto num : nums)
+            if(num < pivot)
+                res.push_back(num);
+        for(const auto num : nums)
+            if(num == pivot)
+                res.push_back(num);
+        for(const auto num : nums)
+            if(num > pivot)
+                res.push_back(num);
+    
+        return res;
+    }
+};
